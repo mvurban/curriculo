ae728487c5096489ebdc1a6bc336f6704ead4b18
Mvurban <marcelo.vendas@iprj.uerj.br>
2022-05-05 12:05:00 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import './App.css';
 
 function App() {
-  return (
-    <div> Olá Mundo !!!!</div>
-  );
+
+
+
+   return (
+      <>
+
+            <header>
+               <h1>Marcelo Vendas Urban</h1>
+               <h2>Evolução de carreira, projetos desafiadores e experiência adiquirida. </h2>
+               <h2>Tecnologias e skills desenvolvidas.</h2>
+            </header>
+
+         <main></main>
+         <footer></footer>
+      </>
+
+   );
 }
 
 export default App;
