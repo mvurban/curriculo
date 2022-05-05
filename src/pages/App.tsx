7438f4af562c34ec10cebfe152447a1207fb7021
Mvurban <marcelo.vendas@iprj.uerj.br>
2022-05-05 15:03:01 -0300

    inicio do main

---
+++ src/pages/App.tsx
@@ -0,0 +1,43 @@
+import './App.scss';
+import '../styles/header-animation.css'
+import clock from '../assets/clock.png';
+
+
+function App() {
+
+   return (
+      <>
+
+         <header>
+            <h1 className='user-name'>Marcelo Vendas Urban</h1>
+            <h3>Evolução de carreira, projetos desafiadores e experiência adiquirida. </h3>
+            <h3>Tecnologias e skills desenvolvidas.</h3>
+            <ul className="header-animated">
+               <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+            </ul>
+         </header>
+
+         <main>
+            <div>
+               <div className='container-left'>
+                  <img src={clock} alt='clock time' />
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, nulla assumenda. Consectetur labore facilis reprehenderit inventore incidunt aspernatur dolores doloremque velit nam nemo, fuga, sapiente magni excepturi, saepe ut. Autem?</p>
+               </div>               
+            </div>
+            <div>meio</div>
+            <div>
+               <h2>Novas <br />Tecnologias</h2>
+               <p>
+                  O momento atual é de aprendizagem e mudanças.
+               </p>
+            </div>
+
+         </main>
+         <footer></footer>
+
+      </>
+
+   );
+}
+
+export default App;
